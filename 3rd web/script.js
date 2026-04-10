2582f2f5ff6bedc9387982c8ea1fc2d3752434e7
Karan Mahato <karanmahato168@gmail.com>
2026-04-11 00:10:22 +0530

    Delete 3rd web directory

--- 3rd web/script.js
+++
@@ -1,6 +0,0 @@
-// Add a shadow to the header when scrolling down
-let header = document.querySelector('header');
-
-window.addEventListener('scroll', () => {
-    header.classList.toggle('sticky', window.scrollY > 100);
-});
